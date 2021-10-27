d7d3877552f18e87562729b0090c97808b01bf14
Nontapan Rujirakal <nontapan.rujirakal@gmail.com>
2021-10-28 01:06:36 +0700

    Add files via upload

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl << b << endl << c;
+    printf("\n%.3f\n%.9lf",d,e);
+    
+    return 0;
+}
